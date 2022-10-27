861d08d2367e728fbe437b5721f6263f9cb0914c
hendrawidjaya <hendrawidjaya90@gmail.com>
2022-10-27 17:03:12 +0200

    change the css and add music

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import NewsSection from "./components/newsSection/news-section";
 import WelcomeSection from "./components/welcomeSection/welcome-section";
+import GetSpacePhotos from "./components/SpacePhotosComponent/space-photos";
 
 function App() {
   return (
     <div className="wrapper">
       <WelcomeSection />
       <NewsSection />
+      <GetSpacePhotos />
     </div>
   );
 }
