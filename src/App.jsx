101f968c5cba498775fe112bbb186ec4fa07a784
hendrawidjaya <hendrawidjaya90@gmail.com>
2022-11-02 20:17:48 +0100

    added the react-router-dom, created pages folder on src, created the Home and About Us pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import "./App.css";
 import NewsSection from "./components/newsSection/news-section";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import GetSpacePhotos from "./components/SpacePhotosComponent/space-photos";
+import Home from "./pages/Home/home";
+import AboutUs from "./pages/AboutUs/about-us";
+
 
 function App() {
-  return <>{<NewsSection />}</>;
+  return (
+      <>
+      <div className="App">
+      <Router>
+        <Routes>
+          <Route element={<Home />} exact path="/" />
+          <Route element={<AboutUs />} path="/about-us" />
+        </Routes>
+      </Router>
+      </div>
+      </>
+  );
 }
 
 export default App;
