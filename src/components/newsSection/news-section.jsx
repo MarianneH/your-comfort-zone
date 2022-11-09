18bf39e20609a2317345542a78f4f7826442dbd4
MarianneH <marianne.helbig@googlemail.com>
2022-11-09 15:10:16 +0100

    made endless-scroll work

--- src/components/newsSection/news-section.jsx
+++ src/components/newsSection/news-section.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import axios from "axios";
 import styles from "./newsSection.module.css";
 import "../../root.css";
@@ -13,12 +13,28 @@ function NewsSection() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [hasMore, setHasMore] = useState(false);
+  const observer = useRef();
+  const lastNewsElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
   const urls = {
     newscatcher: `https://api.newscatcherapi.com/v2/search?q=${query}&lang=en&sources=theguardian.com&page_size=20&page=${pageNumber}`,
   };
 
   async function fetchAPI(url, setResp) {
     setLoading(true);
+    setError(false);
     axios
       .get(url, {
         headers: {
@@ -26,27 +42,38 @@ function NewsSection() {
         },
       })
       .then((response) => {
-        console.log(response.data.total_hits); //check how many results there are
-        setResp((prevResp) => {
-          console.log(...new Set(prevResp));
-          return [...new Set([...prevResp, ...response.data.articles])];
+        let currentData = response.data.articles;
+        currentData.forEach((e) => {
+          delete e._score;
+          delete e.author;
+          delete e.authors;
+          delete e.country;
+          delete e._id;
+          delete e.topic;
+          delete e.twitter_account;
+          delete e.rights;
+          delete e.rank;
+          delete e.published_date_precision;
+          delete e.published_date;
+          delete e.language;
+          delete e.is_opinion;
+          delete e.clean_url;
+          delete e.summary;
         });
-
-        setHasMore(response.data.articles.length > 0);
+        setResp((prevResp) => {
+          return [...new Set([...prevResp, ...currentData])];
+        });
+        setHasMore(response.data.total_hits > data.length);
+        setLoading(false);
       })
       .catch((e) => {
-        console.log("ERROR MESSAGE: " + e);
+        console.log("ERROR MESSAGE : " + e);
       });
   }
 
   useEffect(() => {
     fetchAPI(urls.newscatcher, setData);
-    console.log(pageNumber);
   }, [query, pageNumber]);
-
-  if (data.length === 0) {
-    return <div>no use today</div>;
-  }
 
   return (
     <div className={styles.news_section}>
@@ -58,6 +85,16 @@ function NewsSection() {
       <div className={styles.newsfeed}>
         {data.map((element, index) => (
           <div key={index}>
+            {data.length === index + 1 && (
+              <div ref={lastNewsElementRef} key={index}>
+                <NewsCard
+                  url={element.link}
+                  media={element.media}
+                  title={element.title}
+                  excerpt={element.excerpt}
+                />
+              </div>
+            )}
             {index === 4 && <GetSpacePhotos />}
             {index === 9 && (
               <div className={styles.api_space}> Space for API 2</div>
@@ -65,28 +102,22 @@ function NewsSection() {
             {index === 14 && (
               <div className={styles.api_space}> Space for API 2</div>
             )}
-            <NewsCard
-              key={index}
-              url={element.url}
-              media={element.media}
-              title={element.title}
-              excerpt={element.excerpt}
-            />
+            {data.length !== index + 1 && (
+              <div key={index}>
+                <NewsCard
+                  url={element.link}
+                  media={element.media}
+                  title={element.title}
+                  excerpt={element.excerpt}
+                />
+              </div>
+            )}
           </div>
         ))}
       </div>
+      <div>{loading && "Loading ..."}</div>
+      <div>{error && "Error"}</div>
     </div>
   );
 }
 export default NewsSection;
-
-// function handleChange(event) {
-//   setSearchInput(event.target.value);
-// }
-
-// function handleClick(event) {
-//   event.preventDefault();
-//   setQuery(searchInput);
-//   console.log(searchInput);
-//   setSearchInput("");
-// }
