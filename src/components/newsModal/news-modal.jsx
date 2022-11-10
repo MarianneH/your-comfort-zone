9e5e2be0ecb9dae4d4305bfeff448a84eb4e62f7
MarianneH <marianne.helbig@googlemail.com>
2022-11-10 20:56:20 +0100

    added basic styling and closing functionality

--- src/components/newsModal/news-modal.jsx
+++ src/components/newsModal/news-modal.jsx
@@ -1,32 +1,37 @@
 import styles from "./newsModal.module.css";
 import React from "react";
-import { useEffect } from "react";
 
 function NewsModal({ data, setShowModal }) {
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
-
   function handleClick() {
     setShowModal(false);
   }
   return (
-    <div className={styles.modal_container}>
+    <div className={styles.modal_container} onClick={handleClick}>
       <div className={styles.modal}>
         <div className={styles.modal_header}>
-          <div>In focus</div>
-          <div onClick={handleClick}>&#10005;</div>
+          <div className={styles.header_text}>In focus</div>
+          <div onClick={handleClick} className={styles.header_x}>
+            &#10005;
+          </div>
         </div>
         {data && (
           <div className={styles.modal_content}>
+            <h1 className={styles.headline}>{data.title}</h1>
+            <div className={styles.date}>
+              {data.published_date} - {data.authors}
+            </div>
             <div className={styles.image}>
               <img src={data.media} alt={data.title} />{" "}
             </div>
-            <div className={styles.headline}>{data.title}</div>
-            <div className={styles.date}>{data.published_date}</div>
-            <div className={styles.link}>{data.link}</div>
             <div className={styles.summary}>{data.summary}</div>
-            <div className={styles.author}>{data.authors}</div>
+            <a
+              href={data.link}
+              className={styles.link}
+              target="_blank"
+              rel="noreferrer"
+            >
+              go to original article &#8594;
+            </a>
           </div>
         )}
       </div>
