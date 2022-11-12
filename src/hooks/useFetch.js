44daa1b7b77cb2a5f750b70c39355216ff672c9b
MarianneH <marianne.helbig@googlemail.com>
2022-11-12 09:29:44 +0100

    added comments to useFetch hook on how to use it

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+// url = url to fetch the API incl. API key in url string OR not incl. API key in url string
+// key = mostly empty string "" - only relevant if API key has to be passed in headers (like for the news API)
+// setLoading = has to be a state in parent component using const [loading, setloading] = useState(true)
 function useFetch(url, key, setLoading) {
   const [dataX, setData] = useState(null);
   const [errorX, setError] = useState(null);
--- src/pages/AboutUs/about-us.jsx
+++ src/pages/AboutUs/about-us.jsx
@@ -3,21 +3,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function AboutUs() {
-    return (
-        <div className="about-us">
-            {/* <ul>
+  return (
+    <div className="about-us">
+      {/* <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about-us">About Us</Link></li>
             </ul> */}
-            <h1>Who made this possible</h1>
-            <h2>Marianne</h2>
-            <p>--Marianne Content--</p>
-            <h2>Felipe</h2>
-            <p>--Felipe Content--</p>
-            <h2>Georg</h2>
-            <p>--Georg Content--</p>
-            <h2>Hendra</h2>
-            <p>--Hendra Content--</p>
-        </div>
-    );
+      <h1>Who made this possible</h1>
+      <h2>Marianne</h2>
+      <p>--Marianne Content--</p>
+      <h2>Felipe</h2>
+      <p>--Felipe Content--</p>
+      <h2>Georg</h2>
+      <p>--Georg Content--</p>
+      <h2>Hendra</h2>
+      <p>--Hendra Content--</p>
+    </div>
+  );
 }
