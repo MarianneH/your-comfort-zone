e51630d23bfcd82cc1c66f4c58fa41942e70521e
MarianneH <marianne.helbig@googlemail.com>
2022-11-23 22:36:29 +0100

    need folder name to adapt

---
+++ .netlify/functions/fetch-space.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+
+exports.handler = function (event, context, callback) {
+  axios
+    .get(
+      "https://api.nasa.gov/planetary/apod?api_key=fVqS96C8GmZXNB9Oy4mSHgXQJ0TtQvgTxEQD8yld"
+    )
+    .then(function (d) {
+      console.log(d); // has data here!!
+    })
+    .catch((e) => {
+      // res.status(400).json({ msg: e });
+    });
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
+    },
+    body: JSON.stringify({ message: "Hello World!" }),
+  };
+
+  callback(null, response);
+};
