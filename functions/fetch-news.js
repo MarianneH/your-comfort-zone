ea7a5edbcf9676845786e0a1bd0004fbb80fdea9
MarianneH <marianne.helbig@googlemail.com>
2022-11-30 09:17:24 +0100

    started serverless function for news

--- functions/fetch-news.js
+++ functions/fetch-news.js
@@ -3,22 +3,28 @@ const axios = require("axios");
 exports.handler = async function (event, context) {
   console.log(event);
   console.log(context);
+  console.log(event.queryStringParameters);
 
-  const options = {
-    method: "GET",
-    url: `https://api.newscatcherapi.com/v2/search?q=${query}&lang=en&sources=theguardian.com&page_size=20&page=${pageNumber}`,
-  };
+  //   const query = req.params.query;
+  //   const pageNumber = req.params.pageNumber;
+  //   const options = {
+  //     method: "GET",
+  //     url: `https://api.newscatcherapi.com/v2/search?q=${query}&lang=en&sources=theguardian.com&page_size=20&page=${pageNumber}`,
+  //     headers: {
+  //       "x-api-key": process.env.REACT_APP_NEWS_CATCHER_KEY,
+  //     },
+  //   };
 
-  try {
-    const response = await axios(options);
-    return {
-      statusCode: 200,
-      body: JSON.stringify(response.data),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: error.toString(),
-    };
-  }
+  //   try {
+  //     const response = await axios(options);
+  //     return {
+  //       statusCode: 200,
+  //       body: JSON.stringify(response.data),
+  //     };
+  //   } catch (error) {
+  //     return {
+  //       statusCode: 500,
+  //       body: error.toString(),
+  //     };
+  //   }
 };
